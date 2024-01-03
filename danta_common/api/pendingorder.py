cf49c4749cecf1d046b39a0cfdbaf0729e81f300
王思林 <142557847+wsl2417@users.noreply.github.com>
2024-01-03 11:15:16 +0800

    fix comments one

--- danta_common/api/pendingorder.py
+++ danta_common/api/pendingorder.py
@@ -4,14 +4,12 @@ from Libraries.request_core.rest_client import RestClient
 api_root_url = COMMON_CONFIG["host"]
 
 
-class PenDingOrder(RestClient):
+class PendingOrder(RestClient):
     def __init__(self, api_root_url):
         super(PenDingOrder, self).__init__(api_root_url)
-        # self.base_username = COMMON_COFIG.get("base_username")
-        # self.base_password = COMMON_COFIG.get("base_password")
 
     def get_order_list(self, **kwargs):
         return self.post("/casamiel/api/trade/seller/pending/getOrderList", **kwargs)
 
 
-pending = PenDingOrder(api_root_url)
+pending_order = PendingOrder(api_root_url)
